f0ff9fb7f9e97287ec36d36a0f5a65bc1ca90152
bo wen <18272262790@163.com>
2025-01-13 10:51:58 +0800

    无头双向链表

--- J20250112D/src/MyLinkedList.java
+++ J20250112D/src/MyLinkedList.java
@@ -38,7 +38,21 @@ public class MyLinkedList implements IList{
 
     @Override
     public void addIndex(int index, int data) {
-
+        if(index < 0 || index > size()) {
+            //抛出一个异常
+            return;
+        }
+        ListNode cur = this.head;
+        int count = 0;
+        while (count != index) {
+            cur = cur.next;
+            count++;
+        }
+        //此时cur就在需要替换的位置
+        ListNode last = cur.prev;
+        ListNode add = new ListNode(data);
+        add.next = cur;
+        add.prev = last;
     }
 
     @Override
@@ -53,12 +67,42 @@ public class MyLinkedList implements IList{
 
     @Override
     public void remove(int key) {
-
+        ListNode cur = this.head;
+        ListNode curNext = cur.next;
+        for (int i = 0; i < size(); i++) {
+            cur.prev = null;
+            cur.next = null;
+            cur = curNext;
+            if(cur.next != null)
+            {
+                curNext = curNext.next;
+            }
+        }
     }
 
     @Override
     public void removeAllKey(int key) {
+        if(this.head == null) return;
+        ListNode cur = this.head;
+        ListNode prev = this.head;
+        ListNode next = this.head;
+        while (cur.next != null) {
+            next = cur.next;
+            prev = cur.prev;
+            if(cur.val == key) {
+                cur.next = null;
+                cur.prev = null;
+                prev.next = next;
+                next.prev = prev;
+                cur = next;
 
+            }else {
+                prev = prev.next;
+
+                cur = cur.next;
+            }
+            next = next.next;
+        }
     }
 
     @Override
@@ -84,6 +128,14 @@ public class MyLinkedList implements IList{
 
     @Override
     public void clear() {
+        ListNode cur = head;
+        while (cur.next != null) {
+            ListNode curNext = cur.next;
 
+            cur.next = null;
+            cur.prev = null;
+            cur = curNext;
+        }
+        head = null;
     }
 }
