a1f93f88fbc587fcfe10b43a6f98362176cee329
bo wen <18272262790@163.com>
2025-01-29 19:58:15 +0800

    堆和priorityQueue

--- J20250128D/src/Test.java
+++ J20250128D/src/Test.java
@@ -1,5 +1,17 @@
+import java.util.PriorityQueue;
+
 public class Test {
     public static void main(String[] args) {
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        priorityQueue.offer(5);
+        priorityQueue.offer(6);
+        priorityQueue.offer(10);
+
+        System.out.println(priorityQueue.poll());
+        System.out.println(priorityQueue.poll());
+    }
+
+    public static void main1(String[] args) {
         TestHeap testHeap = new TestHeap();
         int[] array = {27, 15, 19, 18, 28, 34, 65, 49, 25, 37};
         testHeap.initElem(array);
