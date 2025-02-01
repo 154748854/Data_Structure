26c4d3923d3c7ca97526658fe8dfd0df6b59bf73
bo wen <18272262790@163.com>
2025-02-01 12:15:26 +0800

    排序

---
+++ J20250201D/src/Sort.java
@@ -0,0 +1,50 @@
+public class Sort {
+    /**
+     * 插入排序
+     * @param array
+     */
+    public static void insertSort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int tmp = array[i];
+            int j = i-1;
+            for (; j >= 0; j--) {
+                if(array[j] > tmp) {
+                    array[j+1] = array[j];
+                }else {
+                    break;
+                }
+            }
+            array[j+1] = tmp;
+        }
+    }
+    /**
+     * 希尔排序
+     */
+    public static void shellSort(int[] array) {
+        int gap = array.length;
+        while (gap > 1) {
+            gap /= 2;
+            shell(array,gap);
+        }
+    }
+
+    /**
+     * 对每组进行插入排序
+     * @param array
+     * @param gap
+     */
+    public static void shell(int[] array, int gap) {
+        for (int i = gap; i < array.length; i++) {
+            int tmp = array[i];
+            int j = i-gap;
+            for (; j >= 0; j-=gap) {
+                if(array[j] > tmp) {
+                    array[j+gap] = array[j];
+                }else {
+                    break;
+                }
+            }
+            array[j+gap] = tmp;
+        }
+    }
+}
---
+++ J20250201D/src/Test.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+        int[] array = {1,3,5,5,2,63,6};
+        Sort.insertSort(array);
+        System.out.println(Arrays.toString(array));
+    }
+}
